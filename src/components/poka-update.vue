1013b2505d13314009506da6552aec193e06c52d
ssunbob <12933369+ssunbob@users.noreply.github.com>
2020-08-23 16:13:44 +0800

    patch ref

--- src/components/poka-update.vue
+++ src/components/poka-update.vue
@@ -49,6 +49,28 @@ export default {
 			remote = remote[0] * 1000 * 1000 + remote[1] * 1000 + remote[2];
 			return remote > local;
 		},
+		checkEmpty(value) {
+			switch (typeof value) {
+				case 'undefined':
+					return true;
+				case 'string':
+					if (value.replace(/(^[ \t\n\r]*)|([ \t\n\r]*$)/g, '').length == 0) return true;
+					break;
+				case 'boolean':
+					if (!value) return true;
+					break;
+				case 'number':
+					if (0 === value || isNaN(value)) return true;
+					break;
+				case 'object':
+					if (null === value || value.length === 0) return true;
+					for (var i in value) {
+						return false;
+					}
+					return true;
+			}
+			return false;
+		},
 		async fetchNewVersion(currentVersion, isAdmin) {
 			let storageId = `poka-github-check-${new Date().toJSON().slice(0, 10).replace(/-/g, '/')}`
 
@@ -59,7 +81,7 @@ export default {
 					.then(e => { sessionStorage[storageId] = JSON.stringify(e); return e })
 					.catch(e => console.error(e))
 			}
-			if(githubRes !== '[]'){
+			if(!this.checkEmpty(githubRes)){
 				if (this.compareVersion(currentVersion, githubRes[0].tag_name) || this.debug) {
 					this.checkUpadteStatus = i18n.t("settings_update_canUpdate2", { version: githubRes[0].tag_name });
 					if (this.debug)
